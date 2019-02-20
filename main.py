0a9532ef6130ec7eb6970826ee11fa27678797c2
lakovicj <31940189+lakovicj@users.noreply.github.com>
2019-02-20 23:43:13 +0100

    background substraction
    ali ipak losiji rez tkd nista jbg

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def process_videos():
                     _, contours, plato_hierarchy = cv2.findContours(eroded_frame1, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
                     show_plato = frame1.copy()
 
-                    # izdvoj samo plato koji je najveca kontura
+                    # izdvoj samo plato -> max kontura
                     only_plato = max(contours, key=cv2.contourArea)
                     rect = cv2.minAreaRect(only_plato)
                     box = cv2.boxPoints(rect)
@@ -68,19 +68,33 @@ def process_videos():
                     cv2.drawContours(show_plato, [segment_box], 0, (0, 0, 255), 2)
                     #cv2.imshow("plato ", show_plato)
 
+                # background substraction
+                # ipak losi rez tkd nista od ovoga na kraju..............................
+                gray_frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+                gray_frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+                difference_img = cv2.absdiff(gray_frame1, gray_frame2)
+                threshold_img = cv2.adaptiveThreshold(difference_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 5)
+
 
                 # na svakih N frejmova ide detekcija objekata tj pesaka
                 if frame_cnt%7 == 0:
+                    # za detektovane pesake
+                    detected_list = []
                     # kropuj samo plato.. ipak samo segment platoa kako se ne bi obradjivala cela slika
                     # i posle proveravale granice, ovako samo obradjujes ovo
                     # background substraction mi ni ne treba na kraju zapravo, zbog segmenta
                     detection_segment = frame1[segment_top_left[1]:segment_bottom_left[1]+10,segment_bottom_left[0] + 60:segment_bottom_right[0]-70]
+
+                    # samo za prikaz detektovanih posle
+                    detected_img = detection_segment.copy()
+
                     gray_segment = cv2.cvtColor(detection_segment, cv2.COLOR_BGR2GRAY)
                     gray_segment = 255 - gray_segment
 
                     #cv2.imshow("gray_segment", gray_segment)
 
-                    # kao params za blockSize i C koristio sam one sa prvih vezbi -> davale najvecu tacnost, ostali pokusaji svi <80%
+                    # kao params za blockSize i C sa prvih vezbi -> najveca tacnost, ostali pokusaji svi <80%
                     threshold_segment = cv2.adaptiveThreshold(gray_segment, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 5)
 
                     # closing (dilate pa onda erode)
@@ -91,7 +105,7 @@ def process_videos():
                     # detektuj pesake
 
                     _, contours, hierarchy = cv2.findContours(close_segment, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-                    detected_list = []
+
                     for cnt in contours:
                         #print(cnt.shape)
                         (x, y), radius = cv2.minEnclosingCircle(cnt)
@@ -101,7 +115,7 @@ def process_videos():
                             detected_list.append(cnt)
                             people_in_video += 1
 
-                    detected_img = detection_segment.copy()
+
                     cv2.drawContours(detected_img, detected_list, -1, (0, 255, 0), 1)
                     #cv2.imshow("detektovani", detected_img)
                     cv2.waitKey(50)
